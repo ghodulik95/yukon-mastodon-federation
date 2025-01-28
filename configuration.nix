0cf945c20d37156faf17df6c802d1eaa33ebff58
George Hodulik <ghodulik95@gmail.com>
2025-01-28 15:53:19 -0500

    Added to config

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@ in
 
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda1";
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
@@ -55,15 +55,15 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the MATE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.mate.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
-  };
+  }
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
